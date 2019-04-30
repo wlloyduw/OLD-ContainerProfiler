3a08f6a83d27b497f9dc96e514c397a9586e5862
Raymond Schooley <ravschoo@uw.edu>
2019-04-30 09:16:27 -0700

    Sucessfully ran tool with custom workload.  Created 'ContainerProfile/profile_demo/sleep_test/runCustomJobs.txt' with instructions to help anyone wanting to profile their own workloads.

--- profiler_demo/sleep_test/new_process_test/runDockerProfile.sh
+++ profiler_demo/sleep_test/new_process_test/runDockerProfile.sh
@@ -4,7 +4,7 @@ NWELLS=96
 # mount the container profiler directory on host machine into /data directory in container
 # then start the sysbench container
 #DOCKERCMD="docker run -d --rm -v /home/ubuntu/profiler_demo/container_profiler:/data -e NWELLS=$NWELLS sysbench"
-DOCKERCMD="docker run -d --rm -v host_path:/data -e NWELLS=$NWELLS sysbench"
+DOCKERCMD="docker run -d --rm -v /home/ravschoo/Documents/Capstone/Capstone/ContainerProfiler/profiler_demo/sleep_test/new_process_test:/data -e NWELLS=$NWELLS primesieve"
 runCmd=/data/processpack.sh
 deltaT=1
 $DOCKERCMD bash -c "/data/ru_profiler.sh $runCmd $deltaT"
