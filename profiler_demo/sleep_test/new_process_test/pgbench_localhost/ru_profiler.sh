112f6e0f6873c5ec99b1bf551699c6990df5c23b
Raymond Schooley <ravschoo@uw.edu>
2019-07-05 14:19:10 -0700

    Separted pgbench directory into local and remote directories.  Remote is intended to be used with ec2 m4.4xlarge vm and local is for testing with a laptop.  Modified ru_profiler scrips in both pgbench directories to sleep for deltaT minus the time it takes to run rudatall.  This may help to collect more samples

--- profiler_demo/sleep_test/new_process_test/pgbench_localhost/ru_profiler.sh
+++ profiler_demo/sleep_test/new_process_test/pgbench_localhost/ru_profiler.sh
@@ -22,16 +22,32 @@ echo "processname: " $pname
 ppid=$(pgrep -x "${pname}") 
 echo "parent process ID: " $ppid
 
+#create an array to hold the json in main memory and another to hold the date filenames
+#used to write the data at the end of collection.
+#declare -a json
+#index=0
+#declare -a dates
 
 while true; do
   cpids=($(get_cpid $ppid | xargs))
   echo "All the children PIDs: " ${cpids[@]}
   if kill -0 $ppid &> /dev/null; then  
+    #dates[$index]=`date '+%Y_%m_%d__%H_%M_%S'`;
+    #echo "Date=${dates[$index]}"
     today=`date '+%Y_%m_%d__%H_%M_%S'`;
     file_name="$today.json"
+    t1=$(date '+%H%M%S')
     /data/rudataall.sh  > "/data/${file_name}"
-    sleep $deltaT
+    t2=$(date '+%H%M%S')
+    #json[$index]=$(/data/rudataall.sh)
+    #let index=$index+1
+    let sleep_time=$deltaT-$t1-t2
+    sleep $sleep_time
   else
+    #for i in "${#json[@]}"
+    #do
+     #   echo ${json[$i]} > "/data/${dates[$i]}.txt"
+    #done
     break
   fi  
 done
